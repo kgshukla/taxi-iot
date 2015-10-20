bcaa58186f1434745375c6617d78624cb7f25e87
kgshukla <kgshukla@yahoo.com>
2015-10-20 16:47:44 +0800

    minor update

--- taxirouteservice/src/main/java/io/pivotal/data/taxirouteservice/services/TaxiRouteService.java
+++ taxirouteservice/src/main/java/io/pivotal/data/taxirouteservice/services/TaxiRouteService.java
@@ -24,28 +24,7 @@ public class TaxiRouteService {
 	private TaxiRouteRepository repository;
 	
 	public List<RouteData> getTop10Routes() {
-		
-		RouteData rd1 = new RouteData();
-		rd1.setRoute("1-2");
-		rd1.setNumtrips("41");
-		rd1.setStarttime("1am");
-		rd1.setEndtime("2am");
-		RouteData rd2 = new RouteData();
-		rd2.setRoute("2-3");
-		rd2.setNumtrips("42");
-		rd2.setStarttime("2am");
-		rd2.setEndtime("3am");
-		List<RouteData> list = new ArrayList<RouteData>();
-		list.add(rd1);
-		list.add(rd2);
-		
-		TaxiRouteMetrics trm = new TaxiRouteMetrics();
-		trm.setData(list);
-		trm.setKey("LATEST_DATA");
-		repository.save(trm);
-		
 		List<RouteData> top10routes = repository.findOne("LATEST_DATA").getData();
-		
 		return top10routes;
 	}
 	
